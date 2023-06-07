47c241ac8e44815546c1c368b3eb80b5456b59a0
Hossein <hossein.aflaki70@gmail.com>
2023-06-07 11:56:26 +0330

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
+'use client';
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import { store } from '@/redux/store'
+import { Provider } from 'react-redux'
+ 
 const inter = Inter({ subsets: ['latin'] })
+
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +19,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+     <Provider store={store }>
+     {children}
+     </Provider>
+     
+       
+       
+        </body>
     </html>
   )
 }
